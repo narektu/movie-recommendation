36a5f2442ae278cf123880f5dea87730f26065bc
narektu <ntutundzhian@gmail.com>
2025-10-08 10:59:09 +0200

    refactor(dep): refactored dependencies for db_session and re-wrote routers connection

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,32 +6,33 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import AsyncSessionLocal
 from app.utils import security
 from app.schemas.user import UserOut
+from app.repositories.users_repo import UsersRepository
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
    async with AsyncSessionLocal() as session:
       yield session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/users/login")
+DB_Session = Annotated[AsyncSession, Depends(get_db)]
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> UserOut:
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: DB_Session) -> UserOut:
    payload = security.decode_access_token(token)
 
-   if payload is None:
+   if not payload or "sub" not in payload:
       raise HTTPException(
          status_code=status.HTTP_401_UNAUTHORIZED,
          detail="Could not validate credentials",
          headers={"WWW-Authenticate": "Bearer"},
       )
    
+   email: str = payload.get("sub")
+   user = await UsersRepository.get_user_by_email(db, email)
 
-   try:
-      return UserOut(id=1, email="authenticated_user@example.com")
-   except Exception as e:
+   if user is None:
       raise HTTPException(
-         status_code=status.HTTP_401_UNAUTHORIZED,
-         detail="Token payload is invalid",
-         headers={"WWW-Authenticate": "Bearer"},
+         status_code=status.HTTP_404_NOT_FOUND,
+         detail="User not found",
       )
+   return UserOut.from_orm(user)
    
-CurrentUser = Annotated[UserOut, Depends(get_current_user)],
-DB_Session = Annotated[AsyncSession, Depends(get_db)]
+CurrentUser = Annotated[UserOut, Depends(get_current_user)]
--- app/routers/v1/__init__.py
+++ app/routers/v1/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from . import users_router, movies_router, ratings_router
+from . import users, movies, ratings, recommendations 
 
 api_router = APIRouter()
 
-api_router.include_router(users_router, prefix="/users", tags=["users"])
-api_router.include_router(movies_router, prefix="/movies", tags=["movies"])
-api_router.include_router(ratings_router, prefix="/ratings", tags=["ratings"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(movies.router, prefix="/movies", tags=["movies"])
+api_router.include_router(ratings.router, prefix="/ratings", tags=["ratings"])
+api_router.include_router(recommendations.router, prefix="/recommendations", tags=["recommendations"])
